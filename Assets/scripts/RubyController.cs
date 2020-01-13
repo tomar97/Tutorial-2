a866f02850dbd0a408632dbddc63d580461c84ea
tomar97 <yohamsandwich@gmail.com>
2020-01-12 16:14:21 -0800

    Tutorial-2

---
+++ Assets/scripts/RubyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
+        
+        Vector2 position = rigidbody2d.position;
+
+        position.x += 3.0f * horizontal * Time.deltaTime;
+        position.y += 3.0f * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
